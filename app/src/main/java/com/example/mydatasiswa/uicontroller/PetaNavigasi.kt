ba375e6d29b04f3437167cbe994210bf4c009d7d
Ilam Ball <informasiilam@gmail.com>
2025-12-31 06:17:09 +0700

    feat: Add DestinasiDetail file
    Creates the `DestinasiDetail.kt` file within the `uicontroller.route` package. This file defines the navigation destination for the item details screen.

--- app/src/main/java/com/example/mydatasiswa/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/mydatasiswa/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,79 @@
 package com.example.mydatasiswa.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.mydatasiswa.uicontroller.route.DestinasiDetail
+import com.example.mydatasiswa.uicontroller.route.DestinasiEdit
+import com.example.mydatasiswa.uicontroller.route.DestinasiEntry
+import com.example.mydatasiswa.uicontroller.route.DestinasiHome
+import com.example.mydatasiswa.view.DetailScreen
+import com.example.mydatasiswa.view.EditScreen
+import com.example.mydatasiswa.view.EntrySiswaScreen
+import com.example.mydatasiswa.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(navController = navController, modifier = modifier)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = { itemId ->
+                    navController.navigate("${DestinasiDetail.route}/$itemId")
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+
+        composable(
+            route = DestinasiDetail.routeWithArgs,
+            arguments = DestinasiDetail.arguments
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt(DestinasiDetail.siswaIdArg)
+            itemId?.let {
+                DetailScreen(
+                    navigateBack = { navController.popBackStack() },
+                    navigateToEditItem = { navController.navigate("${DestinasiEdit.route}/$it") }
+                )
+            }
+        }
+
+        composable(
+            route = DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            EditScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+    }
+}
