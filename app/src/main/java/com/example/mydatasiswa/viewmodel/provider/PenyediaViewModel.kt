278e9770ead4beab3c5c140fb73efbaeb15d1214
Ilam Ball <informasiilam@gmail.com>
2025-12-31 06:17:03 +0700

    feat: Add DestinasiDetail file
    Creates the `DestinasiDetail.kt` file within the `uicontroller.route` package. This file defines the navigation destination for the item details screen.

--- app/src/main/java/com/example/mydatasiswa/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mydatasiswa/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,39 @@
 package com.example.mydatasiswa.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.mydatasiswa.repositori.AplikasiDataSiswa
+import com.example.mydatasiswa.viewmodel.DetailViewModel
+import com.example.mydatasiswa.viewmodel.EditViewModel
+import com.example.mydatasiswa.viewmodel.EntryViewModel
+import com.example.mydatasiswa.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+    }
+}
