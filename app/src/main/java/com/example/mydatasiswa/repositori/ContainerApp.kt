0f9f8958b46cec32ff8ffefae0a813ec5add49ae
Ilam Ball <informasiilam@gmail.com>
2025-12-31 06:14:28 +0700

    feat: Add dependency injection container
    Creates the `ContainerApp.kt` file within the `repositori` package. This file sets up the dependency injection container for the application.
    
    Key additions include:
    *   `ContainerApp` interface and its `DefaultContainerApp` implementation.
    *   Retrofit and OkHttpClient configuration for networking, including a logging interceptor.
    *   Initialization of `RepositoryDataSiswa`.
    *   `AplikasiDataSiswa` application class to hold the container instance.

--- app/src/main/java/com/example/mydatasiswa/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/mydatasiswa/repositori/ContainerApp.kt
@@ -1,2 +1,55 @@
 package com.example.mydatasiswa.repositori
 
+import android.app.Application
+import com.example.mydatasiswa.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    // IP 10.0.2.2 adalah alamat untuk localhost dari emulator Android
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+        prettyPrint = true
+        isLenient = true
+    }
+
+    private val logging = HttpLoggingInterceptor().apply{
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
+        .client(klien)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .build()
+
+    private val retrofitService: ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(retrofitService)
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container : ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
