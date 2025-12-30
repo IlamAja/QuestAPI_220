d92df80de695f515b18e5da9d1b610d90fe8f009
Ilam Ball <informasiilam@gmail.com>
2025-12-31 06:17:28 +0700

    refactor: Define ServiceApiSiswa interface
    Defines the `ServiceApiSiswa` interface with suspend functions for handling student data.
    
    This includes methods for:
    *   `GET` all students and a student by ID.
    *   `POST` to insert a new student.
    *   `DELETE` to remove a student.
    *   `PUT` to update a student's data.

--- app/src/main/java/com/example/mydatasiswa/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/mydatasiswa/apiservice/ServiceApiSiswa.kt
@@ -1,5 +1,27 @@
 package com.example.mydatasiswa.apiservice
 
-interface ServiceApiSiswa {
+import com.example.mydatasiswa.modeldata.DataSiswa
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @GET("bacaTeman.php")
+    suspend fun getSiswaById(@Query("id") id: Int): List<DataSiswa> // Mengubah ke List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): Response<Void>
+
+    @DELETE("hapusTeman.php")
+    suspend fun deleteSiswa(@Query("id") id: Int): Response<Void>
+
+    @PUT("updateTeman.php")
+    suspend fun updateSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): Response<Void>
 }
