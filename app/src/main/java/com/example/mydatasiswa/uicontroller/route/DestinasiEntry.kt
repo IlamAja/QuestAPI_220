542f4655b14e3a871ba0b0477b87dfaa5cf266d3
Ilam Ball <informasiilam@gmail.com>
2025-12-31 06:15:22 +0700

    feat: Add DestinasiDetail file
    Creates the `DestinasiDetail.kt` file within the `uicontroller.route` package. This file defines the navigation destination for the item details screen.

--- app/src/main/java/com/example/mydatasiswa/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/mydatasiswa/uicontroller/route/DestinasiEntry.kt
@@ -1,4 +1,9 @@
 package com.example.mydatasiswa.uicontroller.route
 
-object DestinasiEntry {
+import com.example.mydatasiswa.R
+import com.example.mydatasiswa.uicontroller.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
